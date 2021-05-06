f6c678980e47379c1e62ecf862b364f59de33579
Rammy Mansour <Rammy92>
2021-05-06 12:16:33 +0200

    my first commit

---
+++ calc.py
@@ -0,0 +1,6 @@
+class Calc:
+    def multiplication(x, y):
+        return x * y
+
+    def division(x, y):
+        return x / y
---
+++ test_calc.py
@@ -0,0 +1,11 @@
+from calc import Calc
+
+
+class TestClac:
+    def test_multiplication(self):
+        assert 15 == Calc.multiplication(3, 5)
+        assert 25 == Calc.multiplication(5, 5)
+
+    def test_division(self):
+        assert 3 == Calc.division(3, 1)
+        assert 2 == Calc.division(8, 4)
